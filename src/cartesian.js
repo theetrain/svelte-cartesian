451150299a007592b26b45f7ae8d2243c00a7232
Enrico Sacchetti <enrico@theetrain.ca>
2024-06-24 00:33:31 -0400

    feat: add cartesian algorithm

--- src/cartesian.js
+++ src/cartesian.js
@@ -1,12 +1,32 @@
-export function product() {
-  Object.values(props).reduce((acc, cur) => {
-    return acc * cur.length
-  }, 1)
-}
+/**
+ *
+ * @param {{[key: string]: any[]}} obj
+ * @returns {{ [key: string]: any }[]}
+ */
+export function getCartesianProduct (obj) {
+  const entries = Object.entries(obj)
 
-export function getCartesianProps(props = {}) {
-  const propNames = Object.keys(props)
-  // const product = 
+  // Base case: if the entries array is empty, return an array with an empty object
+  if (entries.length === 0) {
+    return [{}]
+  }
 
-  // for (let i = 0; )
+  // Recursive case: extract the first key-value pair from the entries
+  const [key, values] = entries[0]
+
+  const remainingProduct = getCartesianProduct(
+    Object.fromEntries(entries.slice(1))
+  )
+
+  const result = []
+
+  for (const value of values) {
+    // Iterate over each combination of the remaining product
+    for (const product of remainingProduct) {
+      // Create a new object with the current key-value pair and merge it with the remaining product
+      result.push({ [key]: value, ...product })
+    }
+  }
+
+  return result
 }
--- tests/cartesian.test.js
+++ tests/cartesian.test.js
@@ -1,11 +1,40 @@
-const { expect, describe, it } = require("vitest")
+import { describe, expect, it } from 'vitest'
+import { getCartesianProduct } from '../src/cartesian'
 
-describe('product', () => {
-  it('returns the product', () => {
-    
+describe('getCartesianProduct', () => {
+  it('returns prop combinations', () => {
+    const propSet1 = {
+      one: ['two', 'three'],
+      four: ['five', 'six']
+    }
+    const propSet2 = {
+      one: ['two', 'three'],
+      four: ['five', 'six', 'seven'],
+      eight: ['nine', 'ten']
+    }
+
+    const results1 = getCartesianProduct(propSet1)
+    const results2 = getCartesianProduct(propSet2)
+
+    expect(results1).toEqual([
+      { one: 'two', four: 'five' },
+      { one: 'two', four: 'six' },
+      { one: 'three', four: 'five' },
+      { one: 'three', four: 'six' }
+    ])
+    expect(results2).toEqual([
+      { one: 'two', four: 'five', eight: 'nine' },
+      { one: 'two', four: 'five', eight: 'ten' },
+      { one: 'two', four: 'six', eight: 'nine' },
+      { one: 'two', four: 'six', eight: 'ten' },
+      { one: 'two', four: 'seven', eight: 'nine' },
+      { one: 'two', four: 'seven', eight: 'ten' },
+      { one: 'three', four: 'five', eight: 'nine' },
+      { one: 'three', four: 'five', eight: 'ten' },
+      { one: 'three', four: 'six', eight: 'nine' },
+      { one: 'three', four: 'six', eight: 'ten' },
+      { one: 'three', four: 'seven', eight: 'nine' },
+      { one: 'three', four: 'seven', eight: 'ten' }
+    ])
   })
-})
-
-describe('getCartesianProps', () => {
-  it('returns the product')
 })
