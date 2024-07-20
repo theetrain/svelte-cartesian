4736962ef85a8d5d0f8b8bbdfb56b83035b9c0b0
Enrico Sacchetti <enrico@theetrain.ca>
2024-07-19 23:20:01 -0400

    test(e2e): add new snapshots for label testing
    For Svelte 4

--- e2e/svelte-4/tests/tests.spec.js
+++ e2e/svelte-4/tests/tests.spec.js
@@ -2,21 +2,30 @@ import { test, expect } from '@playwright/test'
 
 test('default slot', async ({ page }) => {
   await page.goto('localhost:4173/')
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveScreenshot()
 })
 
 test('named slots asChild', async ({ page }) => {
   await page.goto('localhost:4173/named-slots')
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveScreenshot()
 })
 
 test('custom styles', async ({ page }) => {
   await page.goto('localhost:4173/custom-styles')
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveScreenshot()
+})
+
+test('short and long labels', async ({ page }) => {
+  await page.goto('localhost:4173/labels')
+  await expect(page).toHaveScreenshot({ fullPage: true })
+})
+
+test('short and long labels - dark mode', async ({ page }) => {
+  await page.goto('localhost:4173/labels/dark')
+  await expect(page).toHaveScreenshot({ fullPage: true })
+})
+
+test('slotted labels', async ({ page }) => {
+  await page.goto('localhost:4173/labels/custom')
+  await expect(page).toHaveScreenshot({ fullPage: true })
 })
